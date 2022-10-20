8052a913bb30fd6b20c664da26b3ae33658ceed1
Segni <segnih36@gmail.com>
2022-10-20 22:17:16 +0300

    created the app and view and urls

--- resource/urls.py
+++ resource/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('' , views.LandingPage, name="LandingPage"),
     path('names/' , views.names, name="names"),
     path('list/<str:pk>/', views.list, name="list"),
 ]
--- resource/views.py
+++ resource/views.py
@@ -2,28 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def names(request):
-    return HttpResponse('This is Names List')
+    return render(request, 'homepage.html')
 
 def list(request, pk):
     firstnames = "abebe"
     return HttpResponse(firstnames)
 
+def LandingPage(request):
+    return render(request, 'LandingPage.html')
 
-# testing a list renderer
-def add_student(request):
-    if request.method == 'POST':
-        student_list = []
-        student_name = request.POST.getlist('student_name')
-        student_phone = request.POST.getlist('student_phone')
-
-        zipped = zip(student_name,student_phone)
-
-        for student_name,student_phone in zipped:
-            student = Student.objects.create(student_name=student_name,
-                                             student_phone=student_phone)
-            student_list.append(student)
-
-        return render(request,'students/view_all_student.html', {'instances': student_list})
-    else:
-        return render(request,'students/add_student.html')
-
+def listRender(request):
+    list=2
+    context={'list':list}
+    return render(request, 'homepage.html', context)
